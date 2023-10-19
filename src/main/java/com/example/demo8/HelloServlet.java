2db078e1c9b0f97bcaca03328445a0b78c4b346e
nguyenductuyen18 <nguyenductuyen2k4@gmail.com>
2023-10-19 17:05:19 +0700

    hr

--- src/main/java/com/example/demo8/HelloServlet.java
+++ src/main/java/com/example/demo8/HelloServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.annotation.*;
 public class HelloServlet extends HttpServlet {
     private String message;
 
-
     public void init() {
         message = "Hello World!";
     }
